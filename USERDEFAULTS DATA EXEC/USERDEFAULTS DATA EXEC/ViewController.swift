6cf8617eb70248fb643479abb1692a87b4aa4646
Sam LEE <samlee@qtsteam.com>
2017-07-02 02:47:03 +0800

    FirstCreation

---
+++ USERDEFAULTS DATA EXEC/USERDEFAULTS DATA EXEC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  USERDEFAULTS DATA EXEC
+//
+//  Created by Sam LEE on 2/7/2017.
+//  Copyright © 2017 MacTechin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
